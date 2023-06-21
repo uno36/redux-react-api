d6a81080ce093797fccdea8b040f93bfab7fb86e
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-06-21 16:57:43 +0200

    redu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable */ 
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchUsers } from './redux/users/usersSlice';
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+  const { users, isLoading, error } = useSelector((state) => state.users);
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  const userItems = [];
+  for (let i = 0; i < users.length; i++) {
+    const user = users[i];
+    userItems.push(
+      <li key={user.login.uuid}>
+        {user.name.first} {user.name.last}
+      </li>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Users</h1>
+      <ul>{userItems}</ul>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+/* eslint-disable */ 
+import { configureStore } from '@reduxjs/toolkit';
+import usersReducer from './users/usersSlice';
+
+const store = configureStore({
+  reducer: {
+    users: usersReducer,
+  },
+});
+
+export default store;
